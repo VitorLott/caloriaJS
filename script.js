3638288ab8a66eafee51480a93fdebe66d24c1e3
VitorLott <90906138+VitorLott@users.noreply.github.com>
2021-11-12 07:36:16 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+
+const calcular = document.getElementById('calcular');
+
+
+function calorias(){
+    console.log("sadasd");
+    const nome = document.getElementById('nome').value;
+    const peso = document.getElementById('peso').value;
+    const distancia = document.getElementById('distancia').value;
+    const tempo = document.getElementById('tempo').value;
+    const resultado = document.getElementById('resultado');
+
+    if (nome !== '' && distancia !== '' && tempo !== '') {
+
+        const tempo_corte = tempo.split(':');
+        const horas = parseInt(tempo_corte[0]);
+        const minutos = parseInt(tempo_corte[1]);
+        const segundos = parseInt(tempo_corte[2]);
+
+        const tempo_horas = horas + minutos/60 + segundos/3600;
+        const tempo_minutos = horas + minutos*60 + segundos/60;
+
+        const velocidademedia = (distancia/(tempo_horas)).toFixed(2);
+        const pace = (60/velocidademedia).toFixed(2);
+        const gasto = (velocidademedia*peso*0.0175*(tempo_minutos)).toFixed(2);
+           
+        resultado.textContent = `Olá ${nome} ! Você percorreu ${distancia} km em ${tempo} hrs. Isso quer dizer que a sua velocidade média foi de ${velocidademedia} km/h, o que o corresponde a um pace de ${pace}min/km. Com ${peso}kg, você gastou cerca de ${gasto} cal `;
+
+    }else {
+        resultado.textContent = 'Para que o calculo seja efetuado preencha todos os campos.';
+    }
+
+}
+calcular.addEventListener('click', calorias);
